3d754dbf7c0e53139dce89a5b41ab189210b942c
Jitesh's Mac <macbookair@Macbooks-MacBook-Air.local>
2023-03-30 21:31:54 +0530

    final project

--- BEtaskManager/shared/constants.js
+++ BEtaskManager/shared/constants.js
@@ -1,7 +1,7 @@
 module.exports = {
   SECRET_KEY: "superConfidentialToken",
   MONGODB_URI:
-    "mongodb+srv://jiteshs:jiteshs7@cluster0.5h1dazv.mongodb.net/task?retryWrites=true&w=majority",
+    "mongodb+srv://jiteshs:*****@cluster0.5h1dazv.mongodb.net/task?retryWrites=true&w=majority",
   EMAIL_REGEX:
     /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/,
   USER_STATUS: {
